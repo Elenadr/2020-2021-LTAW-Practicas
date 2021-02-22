e5d7a6154c6f7e91be51f6ca8909413f31aaa2b9
elenadr <elenadelriogale@hotmail.com>
2021-02-22 12:22:38 +0100

    Ejemplos node

---
+++ P1/ejemplo-02.js
@@ -0,0 +1,15 @@
+//-- Probando variables y cómo imprimirlas
+//-- en la consola
+
+//-- Variable numérica
+var i = 5; //global
+let n = 3; //solo en el ámbito en el que estas
+
+//-- Imprimir la variable directamente
+console.log("Variable n: ", n);
+
+//-- Valor de la variable dentro de una cadena
+console.log(`Variable n: ${n} metros`);
+
+//-- Concatenar la variable al mensaje
+console.log("Variable n: " + n);
