7f37eafab04370359b91f5583f34147cc2c64476
elenadr <elenadelriogale@hotmail.com>
2021-02-22 12:23:09 +0100

    bucles

---
+++ P1/ejemplo-03.js
@@ -0,0 +1,9 @@
+//-- Ejemplo de bucles
+
+//-- Definiendo una constante: Número de mensajes
+const N = 10; 
+
+//-- Bucle para imprimir N mensajes
+for (i = 0; i < N; i++) {
+    console.log("Mensaje " + i);
+}
