d75defc1bd7e0ecb46f22200fccdd7be24d93d58
elenadr <elenadelriogale@hotmail.com>
2021-03-02 14:46:46 +0100

    modulo fs

--- P1/S03/Ej08-print-req.js
+++ P1/S03/Ej08-print-req.js
@@ -1,70 +1,15 @@
-const http = require('http');
+//-- Importar el módulo FS
+const fs = require('fs');
 
-const PUERTO = 8080;
+console.log("Lectura síncrona de un fichero");
 
-//-- Texto HTML de la página principal
-const pagina_main = `
+//-- Realizar la lectura
+const data = fs.readFileSync('fich1.txt','utf8');
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi tienda</title>
-</head>
-<body style="background-color: lightblue">
-    <h1 style="color: green">MI TIENDA</h1>
-</body>
-</html>
-`
+//-- Esta instrucción se ejecuta una vez terminada
+//-- la lectura síncrona
+console.log("Lectura completada...")
 
-//-- Texto HTML de la página de error
-const pagina_error = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi tienda</title>
-</head>
-<body style="background-color: red">
-    <h1 style="color: white">ERROR!!!!</h1>
-</body>
-</html>
-`
-
-const server = http.createServer((req, res)=>{
-    console.log("Petición recibida!");
-
-    //-- Valores de la respuesta por defecto
-    let code = 200;
-    let code_msg = "OK";
-    let page = pagina_main;
-
-    //-- Analizar el recurso
-    //-- Construir el objeto url con la url de la solicitud
-    const url = new URL(req.url, 'http://' + req.headers['host']);
-    console.log(url.pathname);
-
-    //-- Cualquier recurso que no sea la página principal
-    //-- genera un error
-    if (url.pathname != '/') {
-        code = 404;
-        code_msg = "Not Found";
-        page = pagina_error;
-    }
-
-    //-- Generar la respusta en función de las variables
-    //-- code, code_msg y page
-    res.statusCode = code;
-    res.statusMessage = code_msg;
-    res.setHeader('Content-Type','text/html');
-    res.write(page);
-    res.end();
-});
-
-server.listen(PUERTO);
-
-console.log("Ejemplo 7. Escuchando en puerto: " + PUERTO);
+//-- Mostrar el contenido del fichero
+console.log("Contenido del fichero: \n")
+console.log(data);
