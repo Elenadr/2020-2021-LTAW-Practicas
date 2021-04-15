0a1981b9142bf9a9d153c3925c8f4d9d92082bba
elenadr <elenadelriogale@hotmail.com>
2021-04-15 13:39:38 +0200

    cookies

--- P2/S06/server3.js
+++ P2/S06/server3.js
@@ -1,14 +1,14 @@
+//-- Imprimir parametros en la respuesta
+
 const http = require('http');
 const fs = require('fs');
 const PUERTO = 8080;
 
 //-- Cargar pagina web del formulario
-
-//-- Cargar pagina web del formulario
 const FORMULARIO = fs.readFileSync('form1.html','utf-8');
+
 //-- HTML de la página de respuesta
 const RESPUESTA = fs.readFileSync('form1-resp2.html', 'utf-8');
-
 const server = http.createServer((req, res) => {
 
     //-- Construir el objeto url con la url de la solicitud
@@ -30,18 +30,20 @@ const server = http.createServer((req, res) => {
   
         //-- Reemplazar las palabras claves por su valores
         //-- en la plantilla HTML
+        //-- Me genera variable content
         content = RESPUESTA.replace("NOMBRE", nombre);
         content = content.replace("APELLIDOS", apellidos);
   
-        //-- si el usuario es Chuck Norris se añade HTML extra
+        //-- si el usuario es AnaPoveda Garcia se añade HTML extra
         let html_extra = "";
-        if (nombre=="Chuck" && apellidos=="Norris") {
-           html_extra = "<h2>Chuck Norris no necesita registrarse</h2>";
+        if (nombre=="Hermione" && apellidos=="Granger") {
+           html_extra = "<h2>Hermione Granger no necesita registrarse</h2>";
         }
         content = content.replace("HTML_EXTRA", html_extra);
     }
   
     //-- Enviar la respuesta
+    //-- la respuesta no cambia
     res.setHeader('Content-Type', content_type);
     res.write(content);
     res.end()
