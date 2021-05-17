a6eec0016a5f620168abc46c99e482a1dbf2c489
elenadr <elenadelriogale@hotmail.com>
2021-05-17 20:09:33 +0200

    electron

--- P4/S10/main.js
+++ P4/S10/main.js
@@ -14,8 +14,14 @@ electron.app.on('ready', () => {
 
     //-- Crear la ventana principal de nuestra aplicación
     win = new electron.BrowserWindow({
-        width: 600,  //-- Anchura 
-        height: 400  //-- Altura
+        width: 600,   //-- Anchura 
+        height: 600,  //-- Altura
+
+        //-- Permitir que la ventana tenga ACCESO AL SISTEMA
+        webPreferences: {
+          nodeIntegration: true,
+          contextIsolation: false
+        }
     });
 
   //-- En la parte superior se nos ha creado el menu
